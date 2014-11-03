4b4584f56442d66f240785542c6891f299f6cf71
kota999 <kota99949@gmial.com>
2014-11-03 23:42:05 +0900

    First commit

---
+++ crgo.sh
@@ -0,0 +1,16 @@
+if [ $# -ne 1 ]; then
+    echo "valid number of arguments" $#
+    echo "need number of arguments is "1
+    exit 1
+fi
+
+cat << __EOT__
+    argument is
+    $1
+    vi $1.go
+__EOT__
+
+mkdir src/github.com/user/$1
+vi src/github.com/user/$1/$1.go
+
+exit 0
